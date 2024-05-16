75ad8995204d2ab03d2f80d0c0fc12fab09503e9
IUNW04 <126545849+IUNW04@users.noreply.github.com>
2024-05-16 18:23:24 +0100

    Delete djangoCw directory

--- djangoCw/appone/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Item, ItemType, ItemStatus, Booking
-
-# Register your models here.
-
-admin.site.register(Item)
-admin.site.register(ItemType)
-admin.site.register(ItemStatus)
-admin.site.register(Booking)
